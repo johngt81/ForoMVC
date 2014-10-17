6dd3bfb2600e3395560461a8dde7943938d6b534
Johngt81 <john.gt81@gmail.com>
2014-10-17 14:36:11 -0500

    Add WebClient ForoWeb

---
+++ ForoWebApplication/Controllers/NotifyController.cs
@@ -0,0 +1,26 @@
+﻿using ForoWebApplication.Notifiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForoWebApplication.Controllers
+{
+    public class NotifyController : Controller
+    {
+        [HttpPost]
+        public void ReportEvent(string message)
+        {
+            var notifier = new ForoNotifier();
+            notifier.NotifyEvent(message);
+        }
+
+        [HttpPost]
+        public void WarningEvent(string message)
+        {
+            var notifier = new ForoNotifier();
+            notifier.WarningEvent(message);
+        }
+	}
+}
---
+++ ForoWebApplication/Notifiers/ForoNotifier.cs
@@ -0,0 +1,28 @@
+﻿using ForoWebApplication.Hubs;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForoWebApplication.Notifiers
+{
+    public class ForoNotifier
+    {
+        private Microsoft.AspNet.SignalR.Hubs.IHubConnectionContext<dynamic> _clientNotifier;
+        public ForoNotifier()
+        {
+            _clientNotifier = GlobalHost.ConnectionManager.GetHubContext<ForoHub>().Clients;
+        }
+
+        public void NotifyEvent(string message)
+        {
+            _clientNotifier.All.addMessage("Admin", message);
+        }
+
+        public void WarningEvent(string message)
+        {
+            _clientNotifier.All.warningMessage(message);
+        }
+    }
+}
